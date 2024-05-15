00b3a5e9345c901e63016585184d46a1c6121877
khadidja2005 <mk_brakta@esi.dz>
2024-05-15 17:42:06 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module org.example.demo3 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.example.demo3 to javafx.fxml;
+    exports org.example.demo3;
+}
---
+++ src/main/java/org/example/demo3/HelloController.java
@@ -0,0 +1,19 @@
+package org.example.demo3;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected void onHelloButtonClick2() {
+        welcomeText.setText("vous serez redirigé vers la page de connexion");
+    }
+}

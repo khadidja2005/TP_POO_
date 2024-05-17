89567f9dba3424d1644c80835c8362f32dbda303
Khadidja225 <mk_brakta@esi.dz>
2024-05-18 01:48:37 +0200

    'BO'

--- src/main/java/org/example/demo3/HelloApplication.java
+++ src/main/java/org/example/demo3/HelloApplication.java
@@ -6,12 +6,22 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
+
 public class HelloApplication extends Application {
+    private static Stage primaryStage;
+
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root= FXMLLoader.load(getClass().getResource("fiche_suivie.fxml"));
+        primaryStage = stage; // Initialize the primaryStage variable
+        Parent root = FXMLLoader.load(getClass().getResource("page_acceuil.fxml"));
         stage.setScene(new Scene(root));
         stage.show();
+    }
+
+    public static void loadPage(String fxmlFile) throws IOException {
+        Parent root = FXMLLoader.load(HelloApplication.class.getResource(fxmlFile));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/org/example/demo3/rendez_vous_controllers.java
@@ -0,0 +1,122 @@
+package org.example.demo3;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import java.io.IOException;
+
+public class rendez_vous_controllers {
+    @FXML
+    private Button retour;
+    @FXML
+    private Button creerAtelier;
+    @FXML
+    private Button testsAnamnse;
+    @FXML
+    private Button BO;
+    @FXML
+    private Button Creer_BO;
+    @FXML
+
+    private void initialize() {
+        System.out.println("Rendez-vous controller initialized");
+    }
+
+    @FXML
+    private void HandelButtonAction(ActionEvent e) {
+        try {
+            HelloApplication.loadPage("Type_de_rendez_vous.fxml");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            System.out.println("Error: " + ex.getMessage());
+        }
+    }
+    @FXML
+    private void HandelRetourAction2(ActionEvent e) {
+        try {
+            HelloApplication.loadPage("page_acceuil.fxml");
+        } catch (IOException ex){
+            ex.printStackTrace();
+            System.out.println("Error: " + ex.getMessage());
+        }
+    }
+    @FXML
+    private void OnclickCreerRendezVous ( ActionEvent e) {
+        try {
+            HelloApplication.loadPage("Type_de_rendez_vous.fxml");
+
+        }catch (IOException ex) {
+            ex.printStackTrace();
+            System.out.println("Error: " + ex.getMessage());
+        }
+
+    }
+
+    @FXML
+    private void onclickconsultation (ActionEvent e) {
+        try {
+            HelloApplication.loadPage("prise_rendez_vous.fxml");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.out.println("Error: " + ex.getMessage());
+        }
+    }
+    @FXML
+    private void OnclickSuivi ( ActionEvent e) {
+        try {
+            HelloApplication.loadPage("Creer_Suivi.fxml");
+
+        } catch (IOException ex){
+            ex.printStackTrace();
+            System.out.println("Error: " + ex.getMessage());
+        }
+
+    }
+    @FXML
+    private  void OnClickAtelier (ActionEvent e) {
+        try {
+         HelloApplication.loadPage("Creer_Atelier.fxml");
+        }catch ( IOException ex) {
+            ex.printStackTrace();
+            System.out.println("Error: " + ex.getMessage());
+        }
+    }
+    @FXML
+    private void OnclickTests_Anamnse (ActionEvent e) {
+        try {
+         HelloApplication.loadPage("Liste_BO.fxml");
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+            System.out.println("Error: " + ex.getMessage());
+        }
+    }
+    @FXML
+    private void Onclick_BO (ActionEvent e) {
+        try {
+            HelloApplication.loadPage("BO_info.fxml");
+        }catch(Exception ex) {
+         ex.printStackTrace();
+         System.out.println("Error: " + ex.getMessage());
+        }
+    }
+    @FXML
+    private void retourListe_BO (ActionEvent e) {
+        try {
+         HelloApplication.loadPage("Liste_BO.fxml");
+        } catch(IOException ex) {
+           ex.printStackTrace();
+           System.out.println("Error: " + ex.getMessage());
+        }
+    }
+    @FXML
+    private void OnclickCreerBO () {
+        try {
+       HelloApplication.loadPage("Creer_BO.fxml");
+        } catch(IOException ex) {
+            ex.printStackTrace();
+            System.out.println("Error: " + ex.getMessage());
+        }
+    }
+
+}
